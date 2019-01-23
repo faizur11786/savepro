593ed9f492849bc7ace623e3ef089a9148da4a9d
iamfaizur <faizur11786@gmail.com>
2019-01-23 23:53:00 +0530

    Sign Up and Login function is Done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 
-function signup(){
+function signup_btn(){
     var lform = document.querySelector(".login__form");
     var sform = document.querySelector(".signup__form");
     var form_name = document.querySelector(".form__name");
@@ -16,7 +16,7 @@ function signup(){
 
 
 
-function login(){
+function login_btn(){
     var lform = document.querySelector(".login__form");
     var sform = document.querySelector(".signup__form");
     var form_name = document.querySelector(".form__name");
@@ -36,14 +36,53 @@ function user_signup(){
     var number = document.querySelector(".s_number").value;
     var input_data = {name:name,email:email,password:pass,number:number};
     var user_data = JSON.stringify(input_data);
-    console.log("hello")
     if(name!= "" && email != "" && pass != "" && number != ""){
         localStorage.setItem(email,user_data);
-        setTimeout(alert("Sign Up Success now you can Login"),100);
-        
     }
-     
+}
 
+
+function user_already(){
+    var email = document.querySelector(".s_email").value;
     
+    if(localStorage.getItem(email) != null){
+        var er_input = document.querySelector(".s_email");
+        var btn = document.querySelector(".submit_btn");
+        var er_text = document.querySelector(".error_text").innerHTML = "User Already";
+        er_input.classList.add("input_error");
+        btn.disabled = true;
+    }
+}
 
+function remo_error(){
+    var er_input = document.querySelector(".s_email");
+    er_input.classList.remove("input_error");
+    er_input.value="";
+    var btn = document.querySelector(".submit_btn");
+    var er_text = document.querySelector(".error_text").innerHTML = "";
+    btn.disabled = false;
+}
+
+
+
+function user_login(){
+    var userName = document.querySelector("#username").value;
+    var passWord = document.querySelector("#password").value;
+    var login_input = {userName:userName,passWord:passWord};
+    var login_data = JSON.stringify(login_input);
+    sessionStorage.setItem(username,login_data);
+    var sassi_data = sessionStorage.getItem(username);
+    var user = JSON.parse(sassi_data);
+    if(localStorage.getItem(user.userName)== null){
+        alert("User not Found");
+    }
+    else{
+        if(localStorage.getItem(user.userName).match(user.passWord)){
+            alert("hello World");
+
+        }
+        else{
+            alert("Password not Matched");
+        }
+    }
 }
